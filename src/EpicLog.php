632a80bec7c9d4b60c6af2296a37bd511e033eba
John Fischelli <john.fischelli@gmail.com>
2016-07-19 12:59:04 -0500

    we can support rotating different log files by level

--- src/EpicLog.php
+++ src/EpicLog.php
@@ -5,13 +5,19 @@ namespace EpicLog;
 use Psr\Log\LoggerInterface;
 use Monolog\Logger as Monolog;
 use Monolog\Handler\StreamHandler;
+use Monolog\Handler\RotatingFileHandler;
 use Monolog\Formatter\LineFormatter;
 
+/**
+ * EpicLog - Advanced Monolog configuration for Laravel/Lumen
+ *
+ * @author  John Fischelli <john.fischelli@gmail.com>
+ */
 class EpicLog
 {
     /**
      * Holds the Monolog instance booted by Laravel/Lumen Frameworks
-     * @var [type]
+     * @var \Monolog\Logger
      */
     private $monolog;
 
@@ -23,7 +29,10 @@ class EpicLog
 
     /**
      * Constructor
-     * @param LoggerInterface $log This is the Laravel/Lumen Monolog Interface
+     * The LoggerInterface is injected by the container.
+     * It is the Monolog instance boostratpped by the Laravel/Lumen framework.
+     *
+     * @param Psr\Log\LoggerInterface $log
      */
     public function __construct(LoggerInterface $log)
     {
@@ -59,34 +68,6 @@ class EpicLog
     }
 
     /**
-     * Loops through each Log level defined in $this->levels and creates a StreamHandler class for it
-     *
-     * @return array of \Monolog\StreamHandler instances corresponding to each log level
-     */
-    private function setupStreamHandlersByLevel()
-    {
-        $handlers = [];
-        foreach ($this->levels as $level => $monologStatic) {
-            $handlers[$level] = new StreamHandler(
-                storage_path("/logs/{$level}.log"),
-                $monologStatic,
-                false
-            );
-        }
-        return $handlers;
-    }
-
-    /**
-     * Returns a Monolog\LineFormatter Instance configured like Laravel's default
-     *
-     * @return Monolog\LineFormatter
-     */
-    private function setupFormatter()
-    {
-        return new LineFormatter(null, null, true, true);
-    }
-
-    /**
      * This function gets all Handlers and the Formatter and assigns them onto
      * the underlying Monolog instance that Laravel uses.
      *
@@ -106,5 +87,66 @@ class EpicLog
             // push Stream into Laravel Log Instance
             $this->monolog->pushHandler($handler);
         }
+    }
+
+    /**
+     * Loops through each Log level defined in $this->levels and creates a StreamHandler class for it
+     *
+     * @return array of \Monolog\StreamHandler instances corresponding to each log level
+     */
+    private function setupStreamHandlersByLevel()
+    {
+        $handlers = [];
+        foreach ($this->levels as $level => $monologStatic) {
+            if (config('epiclog.rotate_log_by_level')) {
+                $handlers[$level] = $this->setupRotatingLog($level, $monologStatic);
+            } else {
+                $handlers[$level] = $this->setupNormalLog($level, $monologStatic);
+            }
+        }
+        return $handlers;
+    }
+
+    /**
+     * Setups a rotating StreamHandler for a log.
+     *
+     * @param  string $level         key of the array $this->levels - will be the log filename.
+     * @param  const $monologStatic  log level (RFC 5424)
+     * @return \Monolog\StreamHandler
+     */
+    private function setupRotatingLog($level, $monologStatic)
+    {
+        return new RotatingFileHandler(
+            storage_path("/logs/{$level}.log"),
+            config('epiclog.rotate_log_by_level_num_days'),
+            $monologStatic,
+            false
+        );
+    }
+
+    /**
+     * Setups a normal StreamHandler for a log.
+     *
+     * @param  string $level         key of the array $this->levels - will be the log filename.
+     * @param  const $monologStatic  log level (RFC 5424)
+     * @return \Monolog\StreamHandler
+     */
+    private function setupNormalLog($level, $monologStatic)
+    {
+        return new StreamHandler(
+            storage_path("/logs/{$level}.log"),
+            $monologStatic,
+            false
+        );
+    }
+
+    /**
+     * Returns a Monolog\LineFormatter Instance configured like Laravel's default
+     *
+     * @return Monolog\LineFormatter
+     */
+    private function setupFormatter()
+    {
+        return new LineFormatter(null, null, true, true);
     }
 }
--- src/config/epiclog.php
+++ src/config/epiclog.php
@@ -8,6 +8,19 @@ return [
     'separate_logs_by_level' => true,
 
     /**
+     * If this flag is true, we'll setup a RotatingFileHandler instead of a
+     * default StreamHandler for each of our logs.
+     */
+    'rotate_log_by_level' => false,
+
+    /**
+     * Specify the number of days of logs to keep for the log level files.
+     * NOTE: If rotate_log_by_level is false, it doesn't matter what you
+     * put here - all logs will be kept indefinitely.
+     */
+    'rotate_log_by_level_num_days' => 5,
+
+    /**
      * Allows creation of custom logs
      */
     'logs' => []
